d6c8adebfd669a9a1b91108db34e2a5be4d714c3
Anton Sheiko <antonstendal19@gmail.com>
2024-11-23 06:41:12 +0100

    check out date with datepicker add assertions to HotelSearch

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -1,9 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HotelSearch {
 
@@ -17,7 +21,28 @@ public class HotelSearch {
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class='select2-match']")).click();
         driver.findElement(By.name("checkin")).sendKeys("17/04/2024");
-        driver.findElement(By.name("checkout")).sendKeys("20/04/2024");
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
 
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+        List<String> hotelNames = driver.findElements(By.xpath("//h4//b"))
+                .stream()
+                .map(element -> element.getAttribute("textContent"))
+                .collect(Collectors.toList());
+
+        hotelNames.forEach(System.out::println);
+        
+        Assert.assertEquals("Jumeirah Beach Hotel", hotelNames.get(0));
+        Assert.assertEquals("Oasis Beach Tower", hotelNames.get(1));
+        Assert.assertEquals("Rose Rayhaan Rotana", hotelNames.get(2));
+        Assert.assertEquals("Hyatt Regency Perth", hotelNames.get(3));
     }
 }
